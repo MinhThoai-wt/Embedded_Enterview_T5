5b7e0281a6bd6da4a4a123d9d6d8c6ecbbf54503
Pham Minh Thoai <133029271+MinhThoai-wt@users.noreply.github.com>
2023-05-31 17:53:12 +0700

    Add files via upload

---
+++ B1_Bai_tap_2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void shortList(uint8_t n, uint8_t array[])
+{
+    for(int i=0; i<n-1; i++){
+        int h=0;
+        int k=0;
+        for(int j=0; j<n; j++){
+            if(array[i]==array[j]){
+                k=k+1;
+                if(j<i){
+                    h=1;
+                }
+            }
+        }
+        if(h==0){
+            printf("So %d co %d phan tu \n", array[i], k);
+        }
+    }
+}
+
+int main()
+{
+    uint8_t array[]={2,2,7,8,6,2,8,16,22,15,7,9,10,11,9};
+    uint8_t n=sizeof(array);
+    shortList(n, array);
+    return 0;
+}
